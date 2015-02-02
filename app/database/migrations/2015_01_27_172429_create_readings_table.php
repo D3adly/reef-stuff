4641d8ae46568814f2867949c2a0512d1788b9df
Aurimas Gaizauskas <a.gaizauskas@graymatter.co.uk>
2015-02-02 09:30:47 +0000

    Migration files

---
+++ app/database/migrations/2015_01_27_172429_create_readings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReadingsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('readings', function(Blueprint $table)
+		{
+			$table->increments('id');
+            $table->integer('user_id');
+            $table->float('ph')->nullable();
+            $table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+
+			Schema::drop('readings');
+
+	}
+
+}
